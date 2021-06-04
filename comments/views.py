62535300b570a0a3619254a8e865acab48bedb34
John Long <johnlonggately@gmail.com>
2021-06-04 14:44:55 +0000

    added comment views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentDetailSerializer, CommentSerializer
+# pylint: disable=no-member
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List all comments
+    Create a new comment if authenticated
+    Associate the current logged in user with the comment
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_serializer_context(self):
+        context = super(CommentList, self).get_serializer_context()
+        context.update({'request': self.request})
+        return context
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment
+    update or delete a comment if owner
+    """
+
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def get_serializer_context(self):
+        context = super(CommentDetail, self).get_serializer_context()
+        context.update({'request': self.request})
+        return context
