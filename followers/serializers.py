66e9bb209227553f609510c0089630fc473528f3
John Long <johnlonggately@gmail.com>
2021-06-08 12:06:21 +0000

    created followers app and created models and serializers

---
+++ followers/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Follower
+from django.db import IntegrityError
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'followed', 'created_at', 'followed_name'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
