bf62c89207be2e4779879c41c1170b2a95f7c034
John Long <johnlonggately@gmail.com>
2021-06-08 11:06:46 +0000

    added to views.py added urls.py and added to drf_api/urls.py

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
+# pylint: disable=no-member
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List all likes. Create a like if authenticated.
+    The perform_create method associates the like with the logged in user.
+    """
+
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like. No Update view, as users can only like or unlike a post.
+    Destroy a like, i.e. unlike a post if owner of that like
+    """
+
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
