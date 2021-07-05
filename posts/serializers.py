37596bdfa5817b2ba4c781206461f8e36ccb5a25
John Long <johnlonggately@gmail.com>
2021-07-05 15:39:11 +0000

    added to model and serializer so as to be able to use filters on images later

--- posts/serializers.py
+++ posts/serializers.py
@@ -12,6 +12,15 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.width > 4096:
+            raise serializers.ValidationError('Image width larger than 4096px!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError('Image height larger than 4096px!')
+        return value
+
     class Meta:
         model = Post
         fields = [
