aa2b4e3b9afe6f611d8319a1ba0648e5457207eb
John Long <johnlonggately@gmail.com>
2021-07-26 17:05:00 +0000

    updated profiles seralizer and views

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
-from posts.models import Post
+# from posts.models import Post
 from followers.models import Follower
 # pylint: disable=no-member
 
@@ -8,23 +8,26 @@ from followers.models import Follower
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    posts = serializers.SerializerMethodField()
-    followers = serializers.SerializerMethodField()
-    following = serializers.SerializerMethodField()
+    # posts = serializers.SerializerMethodField()
+    # followers = serializers.SerializerMethodField()
+    # following = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_posts(self, obj):
-        return Post.objects.filter(owner=obj.owner).count()
+    # def get_posts(self, obj):
+    #     return Post.objects.filter(owner=obj.owner).count()
 
-    def get_followers(self, obj):
-        return Follower.objects.filter(followed=obj.owner).count()
+    # def get_followers(self, obj):
+    #     return Follower.objects.filter(followed=obj.owner).count()
 
-    def get_following(self, obj):
-        return Follower.objects.filter(owner=obj.owner).count()
+    # def get_following(self, obj):
+    #     return Follower.objects.filter(owner=obj.owner).count()
 
     def get_following_id(self, obj):
         user = self.context['request'].user
@@ -33,13 +36,21 @@ class ProfileSerializer(serializers.ModelSerializer):
                 owner=user,
                 followed=obj.owner
             ).first()
+            print(following)
+            print()
             return following.id if following else None
         return None
 
     class Meta:
         model = Profile
+        # fields = [
+        #     'id', 'owner', 'is_owner', 'created_at', 'updated_at',
+        #     'name', 'content', 'image', 'posts', 'followers', 'following',
+        #     'following_id', 'posts_count', 'followers_count',
+        #      'following_count'
+        # ]
         fields = [
             'id', 'owner', 'is_owner', 'created_at', 'updated_at',
-            'name', 'content', 'image', 'posts', 'followers', 'following',
-            'following_id'
+            'name', 'content', 'image',
+            'following_id', 'posts_count', 'followers_count', 'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.db.models import Count
 # pylint: disable=no-member
 
 
 class ProfileList(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
+    # queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count(
+            'owner__following', distinct=True
+            )
+        ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['posts_count', 'followers_count', 'following_count']
 
 
 class ProfileDetail(generics.RetrieveAPIView):
