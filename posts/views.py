9b7254f4e43fa698eeeb25a44715c83c56736001
John Long <johnlonggately@gmail.com>
2021-07-26 17:03:15 +0000

    updated posts serializers and views

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,22 @@
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, filters
 from .models import Post
 from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 # pylint: disable=no-member
 
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True),
+        likes_count=Count('likes', distinct=True)
+    ).order_by('created_at')
     serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['likes_count', 'comments_count', 'likes__created_at']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
