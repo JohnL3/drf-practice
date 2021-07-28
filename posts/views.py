6fdda6990536b3cb8b1acca26aa8d0867b9c5cc2
John Long <johnlonggately@gmail.com>
2021-07-28 09:23:11 +0000

    added search and filters

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 # pylint: disable=no-member
 
 
@@ -14,8 +15,17 @@ class PostList(generics.ListCreateAPIView):
         likes_count=Count('likes', distinct=True)
     ).order_by('created_at')
     serializer_class = PostSerializer
-    filter_backends = [filters.SearchFilter]
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [
+                filters.SearchFilter,
+                filters.OrderingFilter,
+                DjangoFilterBackend
+            ]
+    filterset_fields = [
+        'owner__profile',
+        'owner__followed__owner__profile',
+        'likes__owner__profile'
+    ]
+    search_fields = ['owner__username', 'title']
     ordering_fields = ['likes_count', 'comments_count', 'likes__created_at']
 
     def perform_create(self, serializer):
--- profiles/views.py
+++ profiles/views.py
@@ -3,11 +3,23 @@ from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 # pylint: disable=no-member
 
 
 class ProfileList(generics.ListCreateAPIView):
     # queryset = Profile.objects.all()
+    '''
+    owner refers to the field, post refers to the model
+    fot the next two owner refers to the field, and as the model is Follower
+    we cant use it twice so insetead we refer to the realated_name of the fields
+    in the Follower model
+    '''
+    q = Profile.objects.annotate(posts_count=Count('owner__post', distinct=True))
+    print(q.query)
+    q = Profile.objects.filter(owner_id=5)
+    print(q.query)
+    print(q)
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
@@ -16,7 +28,11 @@ class ProfileList(generics.ListCreateAPIView):
             )
         ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'owner__following__followed__profile'
+    ]
     ordering_fields = ['posts_count', 'followers_count', 'following_count']
 
 
